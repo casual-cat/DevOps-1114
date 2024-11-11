340fe1b04f6477d8d7e5c9e55d259d9d5d2ae0f8
Gabi Delcea <vioriiiee@gmail.com>
2024-11-11 13:25:58 +0200

    new

---
+++ new.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+age_input = input("How old are you? ")
+if age_input.isdigit():  # Check if the input is a digit
+    age = int(age_input)
+    if age > 18:
+        print("You are an adult.")
+    else:
+        print("You are a minor.")
+else:
+    print("Invalid input. Please enter a number.")
