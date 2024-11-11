7d04793748e01f6f2ac2d4543221077f41ce5367
Gabi Delcea <vioriiiee@gmail.com>
2024-11-11 14:50:54 +0200

    exerciesssss

---
+++ strings.py
@@ -0,0 +1,62 @@
+#  exercise 5
+# -------------------
+# greeting = "hello world"
+# print("length: ", len (greeting))
+# print("first: ", greeting [0])
+# print("first: ", greeting [-1])
+
+#  exercise 6
+# -------------------
+# first_name= "gabi"
+# last_name= "delcea"
+# full_name= first_name + " " + last_name
+# print(f"full name: ", full_name)
+
+#  exercise 8
+# -------------------
+# quote = "famous quote"
+# print("uppercase: ", quote.upper())
+# print("lowercase: ", quote.lower())
+
+#  exercise 9
+# -------------------
+# word = "python"
+# print("first three: ", word[0:3])
+# print("last three: ", word[-3:])
+# print("reverse: ", word[::-1])
+
+#  exercise 10
+# -------------------
+# words="i love python"
+# new = words.replace("python" , "linux")
+# print(new)
+
+#  exercise 11
+# -------------------
+# text = "brown fox jumps over lazy dog"
+# if "fox" in text:
+#     print("true")
+# else:
+#     print("false")
+
+#  exercise 12
+# -------------------
+# fruits= ["mango", "kiwi", "strawberry"]
+# print("list: ", fruits)
+
+# fruits.append("apple")
+# print("updated: ", fruits)
+
+# fruits.pop(0)
+# print("more updated: ", fruits)
+
+#  exercise 13
+# -------------------
+# pets= ["cat", "parrot", "kittens"]
+# print("first: ", pets[0])
+# print("last: ", pets[-1])
+# print("all: ", len(pets))
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,35 @@
+# exercise 1
+#-------------------
+# name = "gabi"
+# age = 20
+# print(name,age)
+
+# exercise 2
+#-------------------
+# x = 10
+# y = 5
+# sum = x + y
+# multiplying = x * y
+# dividing = x / y
+# minus= x - y
+# print("sum: ", sum)
+# print("multiplying: ", multiplying)
+# print("dividing: ", dividing)
+# print("minus idk how it called: ", minus)
+
+# exercise 3
+#-------------------
+# a = 3
+# b = 7
+# a, b = b, a
+# print("a: ", a)
+# print("b: ", b)
+
+# exercise 4
+#-------------------
+length= float(input ("enter length: "))
+width= float (input ("enter width: "))
+sum= length * width
+print ("area:", sum)
+
+
