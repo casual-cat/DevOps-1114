5435a5113837dff8053da8e5efc49d3e84588d6f
casual-cat <vioriiiee@gmail.com>
2024-11-13 23:27:29 +0200

    home desktop

--- course/data_logging.py
+++ course/data_logging.py
@@ -1,7 +1,7 @@
-#Create a New File and Log Entries with Timestamps: Write a script that creates a new file named journal.txt.
-# type: ignore Prompt the user to enter a few lines of text as 'journal entries.'
-# For each entry, log the current date and time (using the datetime module) and the entry into journal.txt.
-# Each line in the file should have the format: [YYYY-MM-DD HH:MM:SS] Entry Text
+# #Create a New File and Log Entries with Timestamps: Write a script that creates a new file named journal.txt.
+# # type: ignore Prompt the user to enter a few lines of text as 'journal entries.'
+# # For each entry, log the current date and time (using the datetime module) and the entry into journal.txt.
+# # Each line in the file should have the format: [YYYY-MM-DD HH:MM:SS] Entry Text
 
 # import datetime
 # with open("journal.txt", "a") as file:
@@ -11,31 +11,31 @@
 #             break
         
 #         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#         file.write(f"[{current_time}] [{entry}]\n")
+#         file.write("[{current_time}] [{entry}]\n")
 #         print("new entry log added into journal.txt file!")
 
-# f= open("journal.txt", "r")
-# for x in f:
-#     print(x)
+# # f= open("journal.txt", "r")
+# # for x in f:
+# #     print(x)
 
-# f= open("journal.txt", "a")
-# import datetime
-# while True:
-#     entry = input("Enter a journal entry: ") 
-#     if entry.lower() == 'exit':
-#         break
-#     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     f.write(f"[{current_time}] [{entry}]\n")
-#     print("new entry log added into journal.txt file!")
+# # f= open("journal.txt", "a")
+# # import datetime
+# # while True:
+# #     entry = input("Enter a journal entry: ") 
+# #     if entry.lower() == 'exit':
+# #         break
+# #     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# #     f.write(f"[{current_time}] [{entry}]\n")
+# #     print("new entry log added into journal.txt file!")
 
 #count words and lines:
 def counting_entries_and_lines():
     with open("journal.txt", "r") as file:
         lines= file.readlines()
         total_entries= len(lines)
-        print(f"you have {total_entries} entries in your journal")
+        print("you have {total_entries} entries in your journal")
         total_words= sum(len(line.split()) for line in lines)
-        print(f"you have {total_words} words in your journal")
+        print("you have {total_words} words in your journal")
 #add entries:
 def add_entry():
     with open("journal.txt", "a") as file:
@@ -45,7 +45,7 @@ def add_entry():
             if entry.lower() == 'exit':
                 break
             current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            file.write(f"[{current_time}] [{entry}]\n") 
+            file.write("[{current_time}] [{entry}]\n") 
             print("new entry log added into journal.txt file!")
 #add entries after yes or no:
 # def add_entry_after_yes_or_no():
